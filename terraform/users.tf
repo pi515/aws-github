c2e8d2c523da3c1f89753f10474d1da13e8760cf
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-10-08 18:27:13 -0500

    chore: init

---
+++ terraform/users.tf
@@ -0,0 +1,29 @@
+variable "users" {
+    type = map(list(string))
+    default = {
+        ryanemcdaniel = ["admin", "Admin", "maintainer"]
+    }
+}
+
+data "github_user" "users" {
+    for_each = var.users
+    username = each.key
+}
+
+locals {
+    main_force_push_bypassers = [
+        data.github_user.users["ryanemcdaniel"].node_id
+    ]
+    main_dismissal_restrictions = [
+        "/${data.github_user.users["ryanemcdaniel"].username}"
+    ]
+    main_pull_request_bypassers = [
+        "/${data.github_user.users["ryanemcdaniel"].username}"
+    ]
+}
+
+resource "github_membership" "users" {
+    for_each             = element(data.github_user.users[*], 0)
+    username             = each.value.username
+    role                 = element(var.users[each.key], 0)
+}
